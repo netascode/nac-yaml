9b69cac31b3afbe3c9a5734566050963359495bb
Oliver Boehmer <oboehmer@cisco.com>
2025-11-02 10:36:06 +0100

    use pytest parametrization for the longer unit tests

--- tests/unit/test_yaml.py
+++ tests/unit/test_yaml.py
@@ -41,142 +41,189 @@ def test_load_yaml_files(tmpdir: Path) -> None:
     assert data["root"]["children"][0]["name"] == "DEF"
 
 
-def test_merge_dict() -> None:
-    # merge dicts
-    destination: dict[Any, Any] = {"e1": "abc"}
-    source: dict[Any, Any] = {"e2": "def"}
-    result: dict[Any, Any] = {"e1": "abc", "e2": "def"}
-    yaml.merge_dict(source, destination)
-    assert destination == result
-    # merge empty destination dict
-    destination = {"e1": None}
-    source = {"e1": "abc"}
-    result = {"e1": "abc"}
-    yaml.merge_dict(source, destination)
-    assert destination == result
-    # merge empty destination dict nested
-    destination = {"e1": None}
-    source = {"e1": {"e2": "abc"}}
-    result = {"e1": {"e2": "abc"}}
-    yaml.merge_dict(source, destination)
-    assert destination == result
-    # merge empty source dict
-    destination = {"e1": "abc"}
-    source = {"e1": None}
-    result = {"e1": "abc"}
-    yaml.merge_dict(source, destination)
-    assert destination == result
-    # merge empty source dict nested
-    destination = {"e1": {"e2": "abc"}}
-    source = {"e1": None}
-    result = {"e1": {"e2": "abc"}}
-    yaml.merge_dict(source, destination)
-    assert destination == result
-    # merge nested dicts
-    destination = {"root": {"child1": "abc"}}
-    source = {"root": {"child2": "def"}}
-    result = {"root": {"child1": "abc", "child2": "def"}}
-    yaml.merge_dict(source, destination)
-    assert destination == result
-    # append when merging lists
-    destination = {"list": [{"child1": "abc"}]}
-    source = {"list": [{"child2": "def"}]}
-    result = {"list": [{"child1": "abc"}, {"child2": "def"}]}
-    yaml.merge_dict(source, destination)
-    assert destination == result
-    # append when merging lists with duplicate items
-    destination = {"list": [{"child1": "abc"}]}
-    source = {"list": [{"child1": "abc"}]}
-    result = {"list": [{"child1": "abc"}, {"child1": "abc"}]}
-    yaml.merge_dict(source, destination)
-    assert destination == result
-    # make sure that the code doesn't hang when merging lists of lists
-    source = {
-        "switch_link_aggregations": [
+@pytest.mark.parametrize(
+    "destination,source,expected",
+    [
+        (
+            {"e1": "abc"},
+            {"e2": "def"},
+            {"e1": "abc", "e2": "def"},
+        ),
+        (
+            {"e1": None},
+            {"e1": "abc"},
+            {"e1": "abc"},
+        ),
+        (
+            {"e1": None},
+            {"e1": {"e2": "abc"}},
+            {"e1": {"e2": "abc"}},
+        ),
+        (
+            {"e1": "abc"},
+            {"e1": None},
+            {"e1": "abc"},
+        ),
+        (
+            {"e1": {"e2": "abc"}},
+            {"e1": None},
+            {"e1": {"e2": "abc"}},
+        ),
+        (
+            {"root": {"child1": "abc"}},
+            {"root": {"child2": "def"}},
+            {"root": {"child1": "abc", "child2": "def"}},
+        ),
+        (
+            {"list": [{"child1": "abc"}]},
+            {"list": [{"child2": "def"}]},
+            {"list": [{"child1": "abc"}, {"child2": "def"}]},
+        ),
+        (
+            {"list": [{"child1": "abc"}]},
+            {"list": [{"child1": "abc"}]},
+            {"list": [{"child1": "abc"}, {"child1": "abc"}]},
+        ),
+        (
+            {},
             {
-                "switch_ports": [
-                    {"port_id": "7", "serial": "asd"},
-                    {"port_id": "8", "serial": "qwe"},
+                "switch_link_aggregations": [
+                    {
+                        "switch_ports": [
+                            {"port_id": "7", "serial": "asd"},
+                            {"port_id": "8", "serial": "qwe"},
+                        ]
+                    }
                 ]
-            }
-        ]
-    }
-    destination = {}
+            },
+            {
+                "switch_link_aggregations": [
+                    {
+                        "switch_ports": [
+                            {"port_id": "7", "serial": "asd"},
+                            {"port_id": "8", "serial": "qwe"},
+                        ]
+                    }
+                ]
+            },
+        ),
+    ],
+    ids=[
+        "merge_dicts",
+        "merge_empty_destination_dict",
+        "merge_empty_destination_dict_nested",
+        "merge_empty_source_dict",
+        "merge_empty_source_dict_nested",
+        "merge_nested_dicts",
+        "append_when_merging_lists",
+        "append_when_merging_lists_with_duplicates",
+        "merge_lists_of_lists_no_hang",
+    ],
+)
+def test_merge_dict(
+    destination: dict[Any, Any], source: dict[Any, Any], expected: dict[Any, Any]
+) -> None:
     yaml.merge_dict(source, destination)
-    assert destination == source
+    assert destination == expected
 
 
-def test_merge_list_item() -> None:
-    # merge primitive list items
-    destination: list[Any] = ["abc", "def"]
-    source_item: Any = "ghi"
-    result: list[Any] = ["abc", "def", "ghi"]
+@pytest.mark.parametrize(
+    "destination,source_item,expected",
+    [
+        (
+            ["abc", "def"],
+            "ghi",
+            ["abc", "def", "ghi"],
+        ),
+        (
+            ["abc", "def"],
+            "abc",
+            ["abc", "def", "abc"],
+        ),
+        (
+            [{"name": "abc", "map": {"elem1": "value1", "elem2": "value2"}}],
+            {"name": "abc", "map": {"elem3": "value3"}},
+            [
+                {
+                    "name": "abc",
+                    "map": {"elem1": "value1", "elem2": "value2", "elem3": "value3"},
+                }
+            ],
+        ),
+        (
+            [{"name": "abc", "map": {"elem1": "value1", "elem2": "value2"}}],
+            {"name": "abc", "name2": "def", "map": {"elem3": "value3"}},
+            [
+                {
+                    "name": "abc",
+                    "name2": "def",
+                    "map": {"elem1": "value1", "elem2": "value2", "elem3": "value3"},
+                }
+            ],
+        ),
+        (
+            [
+                {
+                    "name": "abc",
+                    "name2": "def",
+                    "map": {"elem1": "value1", "elem2": "value2"},
+                }
+            ],
+            {"name": "abc", "map": {"elem3": "value3"}},
+            [
+                {
+                    "name": "abc",
+                    "name2": "def",
+                    "map": {"elem1": "value1", "elem2": "value2", "elem3": "value3"},
+                }
+            ],
+        ),
+        (
+            [{"name": "abc", "name2": "def"}],
+            {"name": "abc", "name3": "ghi"},
+            [{"name": "abc", "name2": "def"}, {"name": "abc", "name3": "ghi"}],
+        ),
+    ],
+    ids=[
+        "merge_primitive_list_items",
+        "do_not_merge_matching_primitive_list_items",
+        "merge_matching_dict_list_items",
+        "merge_matching_dict_list_items_with_extra_src_attribute",
+        "merge_matching_dict_list_items_with_extra_dst_attribute",
+        "do_not_merge_dict_list_items_with_extra_dst_and_src_attributes",
+    ],
+)
+def test_merge_list_item(
+    destination: list[Any], source_item: Any, expected: list[Any]
+) -> None:
     yaml.merge_list_item(source_item, destination)
-    assert destination == result
-    # do not merge matching primitive list items
-    destination = ["abc", "def"]
-    source_item = "abc"
-    result = ["abc", "def", "abc"]
-    yaml.merge_list_item(source_item, destination)
-    assert destination == result
-    # merge matching dict list items
-    destination = [{"name": "abc", "map": {"elem1": "value1", "elem2": "value2"}}]
-    source_item = {"name": "abc", "map": {"elem3": "value3"}}
-    result = [
-        {
-            "name": "abc",
-            "map": {"elem1": "value1", "elem2": "value2", "elem3": "value3"},
-        }
-    ]
-    yaml.merge_list_item(source_item, destination)
-    assert destination == result
-    # merge matching dict list items with extra src primitive attribute
-    destination = [{"name": "abc", "map": {"elem1": "value1", "elem2": "value2"}}]
-    source_item = {"name": "abc", "name2": "def", "map": {"elem3": "value3"}}
-    result = [
-        {
-            "name": "abc",
-            "name2": "def",
-            "map": {"elem1": "value1", "elem2": "value2", "elem3": "value3"},
-        }
-    ]
-    yaml.merge_list_item(source_item, destination)
-    assert destination == result
-    # merge matching dict list items with extra dst primitive attribute
-    destination = [
-        {"name": "abc", "name2": "def", "map": {"elem1": "value1", "elem2": "value2"}}
-    ]
-    source_item = {"name": "abc", "map": {"elem3": "value3"}}
-    result = [
-        {
-            "name": "abc",
-            "name2": "def",
-            "map": {"elem1": "value1", "elem2": "value2", "elem3": "value3"},
-        }
-    ]
-    yaml.merge_list_item(source_item, destination)
-    assert destination == result
-    # do not merge matching dict list items with extra dst and src primitive attribute
-    destination = [{"name": "abc", "name2": "def"}]
-    source_item = {"name": "abc", "name3": "ghi"}
-    result = [{"name": "abc", "name2": "def"}, {"name": "abc", "name3": "ghi"}]
-    yaml.merge_list_item(source_item, destination)
-    assert destination == result
+    assert destination == expected
 
 
-def test_deduplicate_list_items() -> None:
-    # deduplicate dict list items
-    data: dict[Any, Any] = {"list": [{"name": "abc"}, {"name": "abc"}]}
-    result: dict[Any, Any] = {"list": [{"name": "abc"}]}
+@pytest.mark.parametrize(
+    "data,expected",
+    [
+        (
+            {"list": [{"name": "abc"}, {"name": "abc"}]},
+            {"list": [{"name": "abc"}]},
+        ),
+        (
+            {"list": [{"nested_list": [{"name": "abc"}, {"name": "abc"}]}]},
+            {"list": [{"nested_list": [{"name": "abc"}]}]},
+        ),
+        (
+            {"list": ["abc", "abc"]},
+            {"list": ["abc", "abc"]},
+        ),
+    ],
+    ids=[
+        "deduplicate_dict_list_items",
+        "deduplicate_nested_dict_list_items",
+        "do_not_deduplicate_string_list_items",
+    ],
+)
+def test_deduplicate_list_items(
+    data: dict[Any, Any], expected: dict[Any, Any]
+) -> None:
     yaml.deduplicate_list_items(data)
-    assert data == result
-    # deduplicate nested dict list items
-    data = {"list": [{"nested_list": [{"name": "abc"}, {"name": "abc"}]}]}
-    result = {"list": [{"nested_list": [{"name": "abc"}]}]}
-    yaml.deduplicate_list_items(data)
-    assert data == result
-    # do not deduplicate string list items
-    data = {"list": ["abc", "abc"]}
-    result = {"list": ["abc", "abc"]}
-    yaml.deduplicate_list_items(data)
-    assert data == result
+    assert data == expected
